704f08fcc0fd50664b662eb33db92c8a85462ed1
cod3rkane <jcesar.developer@gmail.com>
2019-11-14 12:02:00 +0100

    change shaders

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,9 +1,11 @@
 #version 410
 
 layout (location = 0) in vec3 vertexPosition;
+layout (location = 1) in vec4 vertexColor;
 
 out vec4 color;
 
 void main() {
   gl_Position = vec4(vertexPosition, 1.0);
+  color = vertexColor;
 }
