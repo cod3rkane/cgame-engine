aedc2f0548aa7e7085455bdbf32131a61cd20bd8
cod3rkane <jcesar.developer@gmail.com>
2019-11-14 22:15:26 +0100

    clean code

--- src/main.c
+++ src/main.c
@@ -29,63 +29,8 @@ int main() {
     Shader defaultShader = LoadShader("src/shaders/vertex.glsl", "src/shaders/fragment.glsl");
     glGenVertexArrays(1, &currentVaoId);
 
-    Mesh mesh;
-    mesh.vertices = (float *)malloc( 12 * sizeof(float));
-    mesh.colors = (float *)malloc( 16 * sizeof(float));
-    mesh.indices = (unsigned int *)malloc(6 * sizeof(unsigned int));
-    mesh.vboId = (unsigned int *)malloc(3 * sizeof(unsigned int *));
-    
-    mesh.vboId[0] = 0; // positions
-    mesh.vboId[1] = 0; // colors
-    mesh.vboId[2] = 0; // indices
-
-    float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
-    };
-
-    float colors[] = {
-        0.368627f, 1.0f, 0.737255f, 0.0f,
-        0.368627f, 1.0f, 0.737255f, 0.0f,
-        0.368627f, 1.0f, 0.737255f, 1.0f,
-        0.368627f, 1.0f, 0.737255f, 1.0f
-    };
-
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,  // first Triangle
-        1, 2, 3   // second Triangle
-    };
-
-    mesh.vertices = vertices;
-    mesh.indices = indices;
-    mesh.colors = colors;
-
-    glGenBuffers(1, &mesh.vboId[0]);
-    glGenBuffers(1, &mesh.vboId[1]);
-    glGenBuffers(1, &mesh.vboId[2]);
-
-    glBindVertexArray(currentVaoId);
-
-    glBindBuffer(GL_ARRAY_BUFFER, mesh.vboId[0]);
-    glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), mesh.vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(defaultShader.locs[LOC_VERTEX_POSITION], 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
-    glEnableVertexAttribArray(defaultShader.locs[LOC_VERTEX_POSITION]);
-
-    glBindBuffer(GL_ARRAY_BUFFER, mesh.vboId[1]);
-    glBufferData(GL_ARRAY_BUFFER, 16 * sizeof(float), mesh.colors, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(defaultShader.locs[LOC_VERTEX_COLOR], 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
-    glEnableVertexAttribArray(defaultShader.locs[LOC_VERTEX_COLOR]);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.vboId[2]);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), mesh.indices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glBindVertexArray(0);
+    Vector4 blue = { 0.219608f, 0.619608f, 0.909804f, 1.0f };
+    Mesh mesh = createRect(&blue);
 
     timer_t timer;
     timer_start(&timer);
