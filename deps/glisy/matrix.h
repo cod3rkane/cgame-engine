9b81279db4b6a19b73e601affbd422c3ac0ddf6f
cod3rkane <jcesar.developer@gmail.com>
2019-11-15 17:01:10 +0100

    add deps

---
+++ deps/glisy/matrix.h
@@ -0,0 +1,9 @@
+#ifndef GLISY_MATRIX_H
+#define GLISY_MATRIX_H
+
+#include "mat2.h"
+#include "mat3.h"
+#include "mat4.h"
+#include "quat.h"
+
+#endif
---
+++ deps/glisy/vector.h
@@ -0,0 +1,8 @@
+#ifndef GLISY_VECTOR_H
+#define GLISY_VECTOR_H
+
+#include "vec2.h"
+#include "vec3.h"
+#include "vec4.h"
+
+#endif
