ab188e509ab76ccebdebd38dde3c6a43bcaf53be
cod3rkane <jcesar.developer@gmail.com>
2019-11-16 12:16:12 +0100

    change vertex

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -10,6 +10,6 @@ uniform mat4 projection;
 out vec4 color;
 
 void main() {
-  gl_Position = projection * view * model * vec4(vertexPosition, 1.0);
+  gl_Position = projection * view * vec4(vertexPosition, 1.0);
   color = vertexColor;
 }
