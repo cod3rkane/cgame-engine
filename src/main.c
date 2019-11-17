45a4da0b6f356a706acabd7472759bb493835c49
cod3rkane <jcesar.developer@gmail.com>
2019-11-17 11:34:42 +0100

    using camera

--- src/main.c
+++ src/main.c
@@ -34,26 +34,18 @@ int main() {
     timer_t timer;
     timer_start(&timer);
 
-    glm_perspective(glm_rad(45.0f), (float)windowWidth / (float)windowHeight, 0.1f, 100.0f, projection);
-    glm_translate(view, (vec3){0.0f, 0.0f, -3.0f});
-
     int frameBufferWidth, frameBufferHeight;
+
+    SetupCamera((vec3){ 0.0f, 0.0f, -10.0f }, (vec3){ 0.0f, 1.0f, 0.0f }, -90.0f, 0.0f);
 
     Vector4 blue = { 0.219608f, 0.619608f, 0.909804f, 1.0f };
     Vector4 pink = { 0.901961f, 0.611765f, 1.0f, 1.0f };
     Vector4 purple = { 0.721569f, 0.556863f, 0.909804f, 1.0f };
     Vector4 magenta = { 0.72549f, 0.658824f, 1.0f, 1.0f };
 
-    Entity testeMeshA = CreateRect(&purple, (vec3){ -300.0f, 0.0f, 0.0f });
-    Entity testeMeshB = CreateRect(&blue, (vec3){ 300.0f, 0.0f, 0.0f });
-    Entity testeMeshC = CreateRect(&pink, (vec3){ 400.0f, 0.0f, 0.0f });
-
-    int MAX_ITEMS = 3000;
-    Entity items[MAX_ITEMS];
-
-    for (int i = 0; i < MAX_ITEMS; i++) {
-        items[i] = CreateRect(i % 2 ? &pink : &magenta, (vec3){ i * 10.0f, i * 2.0f, 0.0f });
-    }
+    Entity testeMeshA = CreateRect(&purple, (vec3){ 0.0f, 0.0f, 0.0f });
+    Entity testeMeshB = CreateRect(&blue, (vec3){ 250.0f, 150.0f, 0.0f });
+    Entity testeMeshC = CreateRect(&pink, (vec3){ 0.0f, 0.0f, 0.0f });
 
     while (!glfwWindowShouldClose(window)) {
         glfwGetFramebufferSize(window, &frameBufferWidth, &frameBufferHeight);
@@ -70,11 +62,6 @@ int main() {
         RotateEntityZ(&testeMeshA, 0.04f);
         RotateEntityZ(&testeMeshB, 0.02f);
         RotateEntityZ(&testeMeshC, 0.02f);
-
-        for (int i = 0; i < MAX_ITEMS; i++) {
-            RotateEntityZ(&items[i], 0.01f);
-            DrawEntity(items[i]);
-        }
 
         DrawEntity(testeMeshA);
         DrawEntity(testeMeshB);
